290b3a3c4a902f5eb469c0bbb8805b988a222150
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:08:01 +0200

    feat (Conjured) : add methods

--- src/main/java/org/acme/domain/Conjured.java
+++ src/main/java/org/acme/domain/Conjured.java
@@ -1,4 +1,25 @@
 package org.acme.domain;
 
-public class Conjured {
+public class Conjured extends Item {
+
+    Conjured(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    void updateQuality() {
+        if (quality > 0 && sellIn >= 0) {
+            quality = quality - 2;
+        } else {
+            quality = quality - 4;
+        }
+    }
+
+    void updateSellIn() {
+        sellIn = sellIn - 1;
+    }
 }
