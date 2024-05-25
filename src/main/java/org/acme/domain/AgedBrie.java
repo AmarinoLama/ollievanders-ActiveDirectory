ec5097d1349168943b6f9932a6eb2937f2c8c5c4
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:07:48 +0200

    feat (AgedBrie) : add methods

--- src/main/java/org/acme/domain/AgedBrie.java
+++ src/main/java/org/acme/domain/AgedBrie.java
@@ -1,4 +1,25 @@
 package org.acme.domain;
 
-public class AgedBrie {
+public class AgedBrie extends Item {
+
+    AgedBrie(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    void updateQuality() {
+        if (sellIn >= 0) {
+            quality = quality + 1;
+        } else {
+            quality = quality + 2;
+        }
+    }
+
+    void updateSellIn() {
+        sellIn = sellIn - 1;
+    }
 }
