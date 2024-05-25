c236bc6601e1ebc8a15dffeb170c9333ce11fd5f
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:20:04 +0200

    feat (Item) : connect with database

--- src/main/java/org/acme/domain/Item.java
+++ src/main/java/org/acme/domain/Item.java
@@ -1,18 +1,22 @@
 package org.acme.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "items")
 public abstract class Item implements Updateable {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
     public int idItem = 0;
 
+    @Column (name = "NAME_ITEM")
     public String name = "";
 
+    @Column (name = "SELL_IN")
     public int sellIn = 0;
 
+    @Column (name = "QUALITY")
     public int quality = 0;
 
     public Item(String name, int sellIn, int quality) {
