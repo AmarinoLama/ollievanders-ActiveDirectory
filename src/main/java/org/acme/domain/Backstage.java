53844991a810bfbcec454db2de41a2fa81dff23b
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:07:56 +0200

    feat (Backstage) : add methods

--- src/main/java/org/acme/domain/Backstage.java
+++ src/main/java/org/acme/domain/Backstage.java
@@ -1,4 +1,44 @@
 package org.acme.domain;
 
-public class Backstage {
+public class Backstage extends Item {
+
+    Backstage(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    void updateQuality() {
+
+        if (sellIn >= 11) {
+            quality = quality + 1;
+        }
+
+        if (sellIn <= 10 && sellIn >= 6) {
+            if (quality < 49) {
+                quality = quality + 2;
+            } else {
+                quality = 50;
+            }
+        }
+
+        if (sellIn <= 5) {
+            if (quality < 48) {
+                quality = quality + 3;
+            } else {
+                quality = 50;
+            }
+        }
+
+        if (sellIn <= 0) {
+            quality = 0;
+        }
+    }
+
+    void updateSellIn() {
+        sellIn = sellIn - 1;
+    }
 }
