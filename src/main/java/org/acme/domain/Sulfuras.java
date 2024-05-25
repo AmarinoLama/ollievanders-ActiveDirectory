20859ad3529a7487eab6c33ffab66928f6edbadb
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:08:14 +0200

    feat (Sulfuras) : add methods

--- src/main/java/org/acme/domain/Sulfuras.java
+++ src/main/java/org/acme/domain/Sulfuras.java
@@ -1,4 +1,11 @@
 package org.acme.domain;
 
-public class Sulfuras {
+public class Sulfuras extends Item {
+
+    Sulfuras(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem(){}
+
 }
