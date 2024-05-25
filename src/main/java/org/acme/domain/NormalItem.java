de8fd46013259c42b1c41f4777123c51f543ab29
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:08:07 +0200

    feat (NormalItem) : add methods

--- src/main/java/org/acme/domain/NormalItem.java
+++ src/main/java/org/acme/domain/NormalItem.java
@@ -1,4 +1,28 @@
 package org.acme.domain;
 
-public class NormalItem {
+public class NormalItem extends Item {
+
+    NormalItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    public void updateItem() {
+        updateSellIn();
+        updateQuality();
+    }
+
+    void updateQuality() {
+
+        if (sellIn >= 0) {
+            quality = quality - 1;
+        } else {
+            quality = quality - 2;
+        }
+
+    }
+
+    void updateSellIn() {
+        sellIn = sellIn - 1;
+    }
 }
+
