ea9fc498063bb20aa30549f3770144d396755ac7
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:31:50 +0200

    feat (Sulfuras) : make it a specialization of item

--- src/main/java/org/acme/domain/Sulfuras.java
+++ src/main/java/org/acme/domain/Sulfuras.java
@@ -1,6 +1,15 @@
 package org.acme.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "SULFURAS")
 public class Sulfuras extends Item {
+
+    @Id
+    @OneToOne
+    @JoinColumn(name="item")
+    private Item item;
 
     Sulfuras(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
