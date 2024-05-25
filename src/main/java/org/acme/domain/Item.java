6abd3d78c243199c346d63d2b7d4d73b666fc8eb
AmarinoLama <amarinolama@gmail.com>
2024-05-25 12:05:15 +0200

    feat (Item) : add methods

--- src/main/java/org/acme/domain/Item.java
+++ src/main/java/org/acme/domain/Item.java
@@ -1,4 +1,23 @@
 package org.acme.domain;
 
-public class Item {
+public abstract class Item implements Updateable {
+
+    public String name;
+
+    public int sellIn;
+
+    public int quality;
+
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder salida = new StringBuilder();
+        salida.append(this.name).append(", ").append(this.sellIn).append(", ").append(this.quality);
+        return salida.toString();
+    }
 }
