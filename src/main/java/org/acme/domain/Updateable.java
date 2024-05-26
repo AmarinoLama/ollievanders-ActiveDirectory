656ec3592167338b2f8aa430f6634842a80c1557
AmarinoLama <amarinolama@gmail.com>
2024-05-26 17:20:36 +0200

    feat (Updateable) : add methods

--- src/main/java/org/acme/domain/Updateable.java
+++ src/main/java/org/acme/domain/Updateable.java
@@ -1,5 +1,18 @@
 package org.acme.domain;
 
 public interface Updateable {
-    void updateItem();
+    public int getSellIn();
+    public void setSellIn(int newSellIn);
+    public int getQuality();
+
+    public default void updateSellIn(){
+        setSellIn(getSellIn() - 1);
+    }
+
+    public void updateQuality();
+
+    public default void updateItem(){
+        updateSellIn();
+        updateQuality();
+    }
 }
