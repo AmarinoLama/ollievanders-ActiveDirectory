67d80c5067542a3f813825e6c13a466098249af3
AmarinoLama <amarinolama@gmail.com>
2024-05-26 16:17:44 +0200

    feat (Item) : add attributes, getters and setters

--- src/main/java/org/acme/domain/Item.java
+++ src/main/java/org/acme/domain/Item.java
@@ -1,12 +1,29 @@
 package org.acme.domain;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "t_items")
 public abstract class Item implements Updateable {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public long id;
+
+    @Column(name = "nombre")
     public String name;
 
+    @Column(name = "sell_in")
     public int sellIn;
 
+    @Column(name = "quality")
     public int quality;
+
+    @Column(name = "tipo")
+    public String tipo;
+
+    public Item() {
+    }
 
     public Item(String name, int sellIn, int quality) {
         this.name = name;
@@ -19,5 +36,45 @@ public abstract class Item implements Updateable {
         StringBuilder salida = new StringBuilder();
         salida.append(this.name).append(", ").append(this.sellIn).append(", ").append(this.quality);
         return salida.toString();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 }
